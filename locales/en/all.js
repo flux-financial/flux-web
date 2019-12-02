12a664e881eee742bff16b4f4554b6e32067382b
Matthew Crossman <crossman@tuta.io>
2019-12-01 18:50:47 -0800

    Convert site text to JSON and auto load them.
    Signed-off-by: Matthew Crossman <crossman@tuta.io>

--- locales/en/all.js
+++ locales/en/all.js
@@ -1,8 +1,30 @@
-module.exports = { 
-	global: require('./global'),
-	index: require('./index'),
-	cities: require('./cities'),
-	vendors: require('./vendors'),
-	taxpayers: require('./taxpayers'),
-	savers: require('./savers')
-};
+var path = require('path');
+var fs = require('fs');
+
+var locale = {};
+
+var directoryPath = path.join(__dirname, '/');
+
+console.log(directoryPath);
+
+var files = fs.readdirSync(directoryPath);
+
+files.forEach(function (file) {
+	// check for json
+	if (path.extname(file) === '.json') {
+		let fileObj = JSON.parse(fs.readFileSync(directoryPath + file));
+
+		locale[path.basename(file, '.json')] = fileObj;
+	}
+});
+
+module.exports = locale;
+
+// module.exports = { 
+// 	global: require('./global'),
+// 	index: require('./index'),
+// 	cities: require('./cities'),
+// 	vendors: require('./vendors'),
+// 	taxpayers: require('./taxpayers'),
+// 	savers: require('./savers')
+// };
