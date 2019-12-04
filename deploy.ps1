5dd8c84ed09298b0089aac0cb8e63bb7cd8b857c
Matthew Crossman <matt@crossman.page>
2019-12-03 18:50:42 -0800

    Env variables & made ps1 platform independent

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,26 @@
+<#
+Deployment script
+
+	Usage: ./deploy.ps1 <your username to log in to the server>
+
+Uses scp (copy over SSH) to copy files from the folders named in $folders and the top-level files named in $files.
+
+It logs in as username@fluxfinancial.net and moves files to /home/public/flux.
+#>
+
+Param(
+    [Parameter(Mandatory=$true)]
+    [String]
+    $user
+)
+
 $folders = "bin", "locales", "public", "routes", "views"
 $files = "app.js", "package.json", "package-lock.json"
 
 ForEach-Object -InputObject $folders {
-	scp.exe -r $_ cross@fluxfinancial.net:/home/public/flux/
+	scp -r $_ $user@fluxfinancial.net:/home/public/flux/
 }
 
 ForEach-Object -InputObject $files {
-	scp.exe $_ cross@fluxfinancial.net:/home/public/flux/
+	scp $_ $user@fluxfinancial.net:/home/public/flux/
 }
